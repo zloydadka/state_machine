cb6b72e08f591b92aeecf7b333edf08873796cb3
Alex <zloydadka@gmail.com>
2013-02-25 09:41:23 +0400

    tests

--- src/test/java/org/zloydadka/StateMachineTest.java
+++ src/test/java/org/zloydadka/StateMachineTest.java
@@ -1,14 +1,15 @@
 package org.zloydadka;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashSet;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,43 +17,42 @@ import org.junit.Test;
 
 public class StateMachineTest {
 
-	private URL book_1;
-	private URL book_2;
-	private String splitters;
+	private URL book_1 = ClassLoader.getSystemResource("dostoevskii_fedor_idiot.txt");
+	private URL book_2 = ClassLoader.getSystemResource("tolstoi_l_n__voina_i_mir_utf8.txt");
+	private String splitters = " ,!-.\n\t\r…«»:©()\"'*/<>=?{}^`˄;„";
+	private BufferedReader book1Reader;
+	private BufferedReader book2Reader;
 
 	@Before
 	public void setUp() throws Exception {
+
+		FileInputStream book1Stream = new FileInputStream(new File(book_1.getFile()));
+		book1Reader = new BufferedReader(new InputStreamReader(book1Stream));
 		
-		book_1 = ClassLoader.getSystemResource("tolstoi_l_n__voina_i_mir_utf8.txt");
-		book_2 = ClassLoader.getSystemResource("book2.txt");
-		splitters = " ,!-.\n\t\r…«»:©()\"'*/<>=?{}^`˄;„";
-
-		System.out.println("Working Directory = " + book_1.getFile());
+		FileInputStream book2Stream = new FileInputStream(new File(book_2.getFile()));
+		book2Reader = new BufferedReader(new InputStreamReader(book2Stream));
 	}
 
 	@Test
-	public final void testStateMachine() {
-		File file = new File(book_1.getFile());
-		FileInputStream stream;
-		try {
-			stream = new FileInputStream(file);
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
-			StateMachine sm = new StateMachine(bufferedReader, splitters);
+	public final void testStateMachine() throws IOException {
+		
 
-			assertEquals(sm.getStates().size(), 71);
-		} catch (FileNotFoundException e) {
-			fail("File not found"); 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			
+			StateMachine sm = new StateMachine(book1Reader, splitters);
 
-		// TODO
+			assertEquals(81, sm.getStates().size());
+			
+			HashSet<String> match = sm.match(book2Reader);
+
+			assertEquals(48200, match.size());
+			
+			assertEquals(true, match.contains("мудрость"));
+			assertEquals(true, match.contains("помеха"));
+			assertEquals(true, match.contains("исповедь"));
+			assertEquals(false, match.contains("темный!"));
+			assertEquals(true, match.contains("темнота"));
+	
 	}
 
-	@Test
-	public final void testMatch() {
-		fail("Not yet implemented"); // TODO
-	}
 
 }
